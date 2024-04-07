3d93565889029c9f0e4fc6b6b630cdd11992fe26
smallT123456 <shaffinmalik9@gmail.com>
2024-04-07 09:04:04 +0500

    Commit 1 - Setting up react project and adding navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Main from './pages/Main'
+import TopBar from './components/TopBar'
+
+const App = () => {
+  return (
+    <div>
+      <TopBar/>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopBar = () => {
+  return (
+    <div className='TopBar'>
+      <h2>EmpowerBits</h2>
+    </div>
+  )
+}
+
+export default TopBar
